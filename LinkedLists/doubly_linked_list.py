ecbdcc4b285b0a3a00e00ead966d7ed506f0ead1
Govind <govindajithkumar@gmail.com>
2020-11-17 11:57:36 -0500

    Creating base file for doubly linkedlist

---
+++ LinkedLists/doubly_linked_list.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+if __name__ == '__main__':
+    pass
