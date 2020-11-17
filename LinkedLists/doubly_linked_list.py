14345acac07590ea33fe1d4513b6fa961101eb6b
Govind <govindajithkumar@gmail.com>
2020-11-17 12:41:00 -0500

    Added more doubly linked list funcs

--- LinkedLists/doubly_linked_list.py
+++ LinkedLists/doubly_linked_list.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -11,6 +14,26 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
+    def insertFront(self, new_node):
+        new_node_front = Node(new_node)
+        if self.head is None:
+            self.head = new_node_front
+            self.tail = new_node_front
+        else:
+            new_node_front.next = self.head
+            self.head.prev = new_node_front
+            self.head = new_node_front
+
+    def printDll(self):
+        temp_iterator = self.head
+        while temp_iterator is True:
+            print(temp_iterator.data)
+            temp_iterator = temp_iterator.next
+
 
 if __name__ == '__main__':
-    pass
+    doubly_linked_list = DoublyLinkedList()
+    doubly_linked_list.insertFront(5)
+    doubly_linked_list.insertFront(8)
+    doubly_linked_list.insertFront(10)
+    doubly_linked_list.printDll()
