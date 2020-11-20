aa198c8a2547b041f8ee03b6d17cbbbf838e6c6b
Govind <govindajithkumar@gmail.com>
2020-11-20 16:09:21 -0500

    Added func to add In the middle of a linked list

--- LinkedLists/doubly_linked_list.py
+++ LinkedLists/doubly_linked_list.py
@@ -32,6 +32,18 @@ class DoublyLinkedList:
             new_node_back.prev = self.tail
             self.tail = new_node_back
 
+    def insertIndex(self, new_node, index):
+        new_node_index = Node(new_node)
+        count = 0
+        curr_start = self.head
+        while curr_start.next:
+            if count == index-1:
+                old_next = curr_start.next
+                curr_start.next = new_node_index
+                new_node_index.next = old_next
+            curr_start = curr_start.next
+            count += 1
+
     # Adding elements to the rear of the list
     def printDll(self):
         temp_iterator = self.head
@@ -54,4 +66,5 @@ if __name__ == '__main__':
     doubly_linked_list.insertBack(11)
     doubly_linked_list.insertBack(99)
     # Printing the list
+    doubly_linked_list.printDll()
     doubly_linked_list.printDll()
